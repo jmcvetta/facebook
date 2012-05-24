4cb84d074d5ce7c595eba301e004c56ce3d1cdfe
Jason McVetta <jason.mcvetta@gmail.com>
2012-05-23 19:36:36 -0700

    Fix underscore error in json structure

--- facebook.go
+++ facebook.go
@@ -4,15 +4,14 @@
 // This code was inspired by Sunil Arora's Python implementation:
 // http://sunilarora.org/parsing-signedrequest-parameter-in-python-bas
 
-
 // Package facebook provides utilities for working with the Facebook API
 package facebook
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -27,20 +26,19 @@ var SignedRequestParseError = errors.New("Could not parse Facebook signed_reques
 // SignedRequest is a Facebook data transfer structure.  See 
 // http://developers.facebook.com/docs/authentication/signed_request/ for crappy documentation.
 type SignedRequest struct {
-	Code       string      // An OAuth Code which can be exchanged for a valid user access token via a subsequent server-side request
-	Algorithm  string      // A JSON string containing the mechanism used to sign the request, normally: HMAC-SHA256.
-	IssuedAt   string      // A JSON number containing the Unix timestamp when the request was signed.
-	UserId     string      // A JSON string containing the User ID of the current user.
-	User       interface{} // A JSON object containing the locale string, country string and the age object (containing the min and max number range of the age) for the current user.
-	OauthToken string      // A JSON string that can be used when making requests to the Graph API. This is also known as a user access token.
-	Expires    string      // A JSON number containing the Unix timestamp when the oauth_token expires.
-	AppData    string      // A JSON string containing the content of the app_data query string parameter which may be passed if the app is being loaded within a Page Tab.
-	Page       interface{} // A JSON object containing the page id string, the liked boolean (set to true if the user has liked the page, false if not) and the admin boolean (set to true if the user is an admin of the page, false if they're not). This field is only present if your app is being loaded within a Page Tab.
+	Code        string      // An OAuth Code which can be exchanged for a valid user access token via a subsequent server-side request
+	Algorithm   string      // A JSON string containing the mechanism used to sign the request, normally: HMAC-SHA256.
+	Issued_At   int64       // A JSON number containing the Unix timestamp when the request was signed.
+	User_Id     string      // A JSON string containing the User ID of the current user.
+	User        interface{} // A JSON object containing the locale string, country string and the age object (containing the min and max number range of the age) for the current user.
+	Oauth_Token string      // A JSON string that can be used when making requests to the Graph API. This is also known as a user access token.
+	Expires     int64       // A JSON number containing the Unix timestamp when the oauth_token expires.
+	App_Data    string      // A JSON string containing the content of the app_data query string parameter which may be passed if the app is being loaded within a Page Tab.
+	Page        interface{} // A JSON object containing the page id string, the liked boolean (set to true if the user has liked the page, false if not) and the admin boolean (set to true if the user is an admin of the page, false if they're not). This field is only present if your app is being loaded within a Page Tab.
 }
 
-
 func pad(s string) string {
-	padding := (4 - len(s) % 4) % 4
+	padding := (4 - len(s)%4) % 4
 	return s + strings.Repeat("=", padding)
 }
 
@@ -49,7 +47,6 @@ func base64UrlDecode(s string) ([]byte, error) {
 	b, err := base64.URLEncoding.DecodeString(s)
 	return b, err
 }
-
 
 func ParseSignedRequest(req *http.Request, secret string) (SignedRequest, error) {
 	var err error
